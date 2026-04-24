69c034e03561287b237723044382d090676f11d3
yassercuevas37-cpu <yassercuevas37@gmail.com>
2026-04-24 09:33:53 +0800

    Add files via upload

---
+++ student.js
@@ -0,0 +1,9 @@
+let name = "Your Name";
+let course = "Web Development";
+let year = "3nd Year";
+let favLang = "JavaScript";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Year:", year);
+console.log("Favorite Language:", favLang);
